0b77f0ff04a0f49df801f8187b8454e72b42bd4b
Shane <shane@wildrideapps.com>
2015-11-01 08:03:35 -0800

    Edited 'partial_generator.rb'

--- lib/generators/bootstrap/partial/partial_generator.rb
+++ lib/generators/bootstrap/partial/partial_generator.rb
@@ -5,13 +5,14 @@ module Bootstrap
     class PartialGenerator < ::Rails::Generators::Base
       source_root File.expand_path("../templates", __FILE__)
       desc "This generator generates bootstrap HTML partials"
-      argument :component_name, :type => :string, :default => "application",
+      argument :component_name, :type => :string, :default => "login",
                :banner => "navbar, navbar-devise, carousel"
 
       attr_reader :app_name
 
       def generate_partial
         app = ::Rails.application
+        @app_name = app.class.to_s.split("::").first
         ext = app.config.generators.options[:rails][:template_engine] || :erb
         copy_file "_#{component_name}.html.#{ext}", "app/views/shared/_#{component_name}.html.#{ext}"
       end
