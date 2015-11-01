7aec8279285df2f28a14aef15b65f0b57c0289ba
Shane <shane@wildrideapps.com>
2015-11-01 08:50:50 -0800

    Added statics_generator and about.html.erb

---
+++ lib/generators/bootstrap/statics/statics_generator.rb
@@ -0,0 +1,21 @@
+require 'rails/generators'
+
+module Bootstrap
+  module Generators
+    class StaticGenerator < ::Rails::Generators::Base
+      source_root File.expand_path("../templates", __FILE__)
+      desc "This generator generates bootstrap HTML static pages"
+      argument :component_name, :type => :string, :default => "about",
+               :banner => "navbar, navbar-devise, carousel"
+
+      attr_reader :app_name
+
+      def generate_partial
+        app = ::Rails.application
+        @app_name = app.class.to_s.split("::").first
+        ext = app.config.generators.options[:rails][:template_engine] || :erb
+        copy_file "_#{component_name}.html.#{ext}", "app/views/statics/#{component_name}.html.#{ext}"
+      end
+    end
+  end
+end
